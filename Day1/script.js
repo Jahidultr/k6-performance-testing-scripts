aece083974746a6af911b3d83ce6ec1cc9f42c78
Jahidul Islam <jahidulislamdiu02@gmail.com>
2026-07-19 18:06:15 +0600

    First Script

---
+++ Day1/script.js
@@ -0,0 +1,35 @@
+// import http from "k6/http";
+// import { check, sleep } from "k6";
+
+// export const options = {
+//     vus: 10,
+//     duration: "30s",
+// };
+
+// export default function () {
+//     const res = http.get("https://dummyjson.com/products");
+
+//     check(res, {
+//         "Status 200": (r) => r.status === 200,
+//         "Response < 500ms": (r) => r.timings.duration < 500,
+//     });
+
+//     sleep(1);
+// }
+
+import http from "k6/http";
+
+import { check, sleep } from "k6";;
+export const options = {
+    vus: 10,
+    duration: "30s",
+};
+export default function () {
+    const res = http.get("https://dummyjson.com/products");
+    check(res, {
+        "Status 200": (r) => r.status === 200,
+        "Response <500ms": (r) => r.timings.duration < 500,
+
+    });
+    sleep(1);
+}
