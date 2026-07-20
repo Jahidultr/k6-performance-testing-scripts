7988062c6e85b0caae6e2e83ee7f75349a95d7b6
Jahidul Islam <jahidulislamdiu02@gmail.com>
2026-07-20 17:37:02 +0600

    Post Api

--- Day2/postapi.js
+++ Day2/postapi.js
@@ -1,19 +1,40 @@
+// Goal this repo
+// http.post()
+// JSON Request Body
+// Headers
+// Response Validation
+// Performance Analysis
+
+
 import http from "k6/http";
 import { check, sleep } from "k6";
 
 export const options = {
     vus: 10,
-    duration: "30s"
+    duration: "30s",
 };
 
 export default function () {
-    const res = http.get("https://dummyjson.com/products");
+    const url = "https://dummyjson.com/auth/login";
+
+    const payload = JSON.stringify({
+        username: "emilys",
+        password: "emilyspass",
+        expiresInMins: 30,
+    });
+
+    const params = {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    const res = http.post(url, payload, params);
 
     check(res, {
-        "Status is 200": (r) => r.status === 200,
-        "Response time < 500ms": (r) => r.timings.duration < 500,
+        "Login successful": (r) => r.status === 200,
+        "Response time < 1000ms": (r) => r.timings.duration < 1000,
     });
 
     sleep(1);
 }
-
